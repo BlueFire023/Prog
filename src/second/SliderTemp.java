7f4191073b1206d62d03d30b31428a5b5d567ec4
MoRitZ516 <moritz.binne@gmail.com>
2023-04-06 14:31:18 +0200

    SliderTemp DONE (custom sliders idk, move other slider idk)

--- src/second/SliderTemp.java
+++ src/second/SliderTemp.java
@@ -15,34 +15,33 @@ public class SliderTemp extends JFrame implements ChangeListener
     JSlider sliderCelcius, sliderFahrenheit;
     JTextField textCelsius, textFahrenheit;
 
-    int fahreTemp;
-    int celsiTemp;
-
     public SliderTemp()
     {
         setTitle(" Konvertiere Temperaturen! ");
         setDefaultCloseOperation( EXIT_ON_CLOSE );
 
         sliderCelcius = new JSlider(SwingConstants.VERTICAL, -273, 100, 0);
-        sliderCelcius.setMajorTickSpacing(100);
-        sliderCelcius.setMinorTickSpacing(50);
+        sliderCelcius.setMajorTickSpacing(16);
+        sliderCelcius.setMinorTickSpacing(8);
         sliderCelcius.setPaintTicks(true);
         sliderCelcius.setPaintLabels(true);
         sliderCelcius.setPreferredSize(new Dimension(300,500));
         sliderCelcius.addChangeListener(this);
         sliderCelcius.setName("Celsius-Skala");
+        sliderCelcius.setBorder(BorderFactory.createTitledBorder("Celsius"));
         textCelsius = new JTextField(4);
         textCelsius.setText(sliderCelcius.getValue() + "");
         textCelsius.setToolTipText("Temperatur in Celsius");
 
         sliderFahrenheit = new JSlider(SwingConstants.VERTICAL, -459, 212, 32);
-        sliderFahrenheit.setMajorTickSpacing(100);
-        sliderFahrenheit.setMinorTickSpacing(50);
+        sliderFahrenheit.setMajorTickSpacing(28);
+        sliderFahrenheit.setMinorTickSpacing(14);
         sliderFahrenheit.setPaintTicks(true);
         sliderFahrenheit.setPaintLabels(true);
         sliderFahrenheit.setPreferredSize(new Dimension(300,500));
         sliderFahrenheit.addChangeListener(this);
         sliderFahrenheit.setName("Fahrenheit-Skala");
+        sliderFahrenheit.setBorder(BorderFactory.createTitledBorder("Fahrenheit"));
         textFahrenheit = new JTextField(4);
         textFahrenheit.setText(sliderFahrenheit.getValue() + "");
         textFahrenheit.setToolTipText("Temperatur in Fahrenheit");
@@ -71,12 +70,13 @@ public class SliderTemp extends JFrame implements ChangeListener
         if (quelle.getName().equals("Celsius-Skala"))
         {
             textCelsius.setText(quelle.getValue() + "");
-            textFahrenheit.setText(String.valueOf(( quelle.getValue() * 1.8) + 32));
+            textFahrenheit.setText(String.valueOf(Integer.valueOf((int) (( quelle.getValue() * 1.8) + 32))));
         }
         if (quelle.getName().equals("Fahrenheit-Skala"))
         {
             textFahrenheit.setText(quelle.getValue() + "");
             textCelsius.setText(String.valueOf((( quelle.getValue()-32 ) * 5 ) / 9));
+            sliderCelcius =
         }
     }
 }
