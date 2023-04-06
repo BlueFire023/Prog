a5ae6e8c7b9107f1332a63596f201e21e2455bbd
Denis Schaffer <schaffer.denis03@gmail.com>
2023-04-06 22:30:05 +0200

    DrehSafe dokumentiert (feat chatGPT)

--- src/second/DrehSafe.java
+++ src/second/DrehSafe.java
@@ -11,10 +11,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DrehSafe extends JFrame implements ActionListener, Runnable {
+    // Das korrekte Passwort, um den Safe zu öffnen
     private final String PASSWORD = "8224725301";
+    // Das vom Benutzer eingegebene Passwort
     private String pTest = "";
+    // Der aktuelle Zählerstand des Drehknopfs
     private int counter = 9;
+    // Die Drehrichtung des Drehknopfs
     private boolean clockwise = true;
+    // Die 10 Ziffern-Tasten des Drehknopfs
     private final JButton[] buttons = {
             new JButton("0"),
             new JButton("1"),
@@ -29,14 +34,19 @@ public class DrehSafe extends JFrame implements ActionListener, Runnable {
     };
 
     public DrehSafe() {
+        // Konfiguriere das Fenster
         setTitle("DrehSafe");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         getContentPane().setLayout(new GridLayout(4, 3));
+
+        // Konfiguriere die Tasten des Drehknopfs
         for (JButton button : buttons) {
             button.setBackground(Color.green);
             button.setFont(new Font("Courier", Font.BOLD, 34));
             button.addActionListener(this);
         }
+
+        // Konfiguriere die Anordnung der Tasten im Fenster
         int[] gridArrangement = {0, 1, 2, 9, -1, 3, 8, -1, 4, 7, 6, 5};
         for (int k : gridArrangement) {
             if (k == -1) {
@@ -45,10 +55,13 @@ public class DrehSafe extends JFrame implements ActionListener, Runnable {
                 getContentPane().add(buttons[k]);
             }
         }
+
+        // Starte den Drehknopf im Hintergrund
         new Thread(this).start();
     }
 
     public static void main(String[] args) {
+        // Erstelle ein neues DrehSafe-Objekt und zeige es an
         DrehSafe frm = new DrehSafe();
         frm.setSize(500, 500);
         frm.setVisible(true);
@@ -56,21 +69,29 @@ public class DrehSafe extends JFrame implements ActionListener, Runnable {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // Füge die Benutzereingabe an die bisher eingegebene Kombination an
         pTest += ((JButton) e.getSource()).getText();
         if (PASSWORD.startsWith(pTest)) {
+            // Der Benutzer hat das korrekte Passwort teilweise eingegeben
+            // Setze die Farbe aller Tasten auf grün
             for (JButton button : buttons) {
                 button.setBackground(Color.green);
             }
             if (pTest.equals(PASSWORD)) {
+                // Der Benutzer hat das korrekte Passwort vollständig eingegeben
+                // Beende das Programm
                 System.exit(0);
             }
             clockwise = true;
         } else if (e.getActionCommand().equals(PASSWORD.substring(0, 1))) {
+            // Der Benutzer hat die erste Ziffer des Passworts eingegeben
             pTest = PASSWORD.substring(0, 1);
             clockwise = true;
         } else {
+            // Die Benutzereingabe stimmt nicht mit dem Passwort überein
             pTest = "";
             clockwise = false;
+            // Setze die Farbe aller Tasten auf rot
             for (JButton button : buttons) {
                 button.setBackground(Color.red);
             }
@@ -88,16 +109,21 @@ public class DrehSafe extends JFrame implements ActionListener, Runnable {
     }
 
     public void rotate() throws InterruptedException {
+        // Warte eine Sekunde, bevor die Zahlen rotiert werden
         Thread.sleep(1000);
+        // Wenn clockwise true ist, rotiere die Zahlen im Uhrzeigersinn
         if (clockwise) {
             for (JButton button : buttons) {
                 counter = Integer.parseInt(button.getText());
+                // Reduziere den Zähler um 1, aber wenn der Zähler 0 ist, setze ihn auf 9
                 counter = counter <= 0 ? 9 : counter - 1;
                 button.setText("" + counter);
             }
         } else {
+            // Wenn clockwise false ist, rotiere die Zahlen gegen den Uhrzeigersinn
             for (JButton button : buttons) {
                 counter = Integer.parseInt(button.getText());
+                // Erhöhe den Zähler um 1, aber wenn der Zähler 9 ist, setze ihn auf 0
                 counter = counter >= 9 ? 0 : counter + 1;
                 button.setText("" + counter);
             }
