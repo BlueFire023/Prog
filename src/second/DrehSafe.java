16957df62f52fd1c8e89919066d94e1f8cdcdf12
MoRitZ516 <moritz.binne@gmail.com>
2023-04-06 16:37:56 +0200

    DrehSafe safe ist da, dreh fehlt

--- src/second/DrehSafe.java
+++ src/second/DrehSafe.java
@@ -10,32 +10,69 @@ import java.awt.event.ActionListener;
  * @version 1, 06/04/2023
  */
 
-public class DrehSafe extends JFrame implements ActionListener
+public class DrehSafe extends JFrame implements ActionListener, Runnable
 {
-    /*Schreiben Sie das GUI-Programm mit den zehn Knöpfen für einen Safe aus der letzten Woche neu in Swing:
-    Nun soll eine Kombination von 10 Knopfdrücken (8-2-2-4-7-2-5-3-0-1) das Schloss öffnen, also das Programm beenden.
-    Die Knöpfe sollen aber diesmal wie in der Abbildung ringförmig angeordnet sein.
-
-    Zusätzlich soll sich die Beschriftung (und damit die Wirkung) der Knöpfe im Sekundentakt um eine Position drehen, zunächst nach rechts.
+    /*Zusätzlich soll sich die Beschriftung (und damit die Wirkung) der Knöpfe im Sekundentakt um eine Position drehen, zunächst nach rechts.
     Bei jedem falschen Knopfdruck ändert sich allerdings die Drehrichtung.*/
+
+    private final String PASSWORD = "8224725301";
+    private String tester = "";
+
+    private final JButton[] numPad = {
+                new JButton("0"),
+                new JButton("1"),
+                new JButton("2"),
+                new JButton("3"),
+                new JButton("4"),
+                new JButton("5"),
+                new JButton("6"),
+                new JButton("7"),
+                new JButton("8"),
+                new JButton("9")
+    };
 
     public DrehSafe()
     {
         setTitle(" DrehSafe ");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        setLayout(new GridLayout(4,3));
+        getContentPane().setLayout(new GridLayout(4, 3));
+
+        for (JButton knopf : numPad) {
+            knopf.setFont(new Font("Courier", Font.BOLD, 34));
+            knopf.addActionListener(this);
+        }
+
+        getContentPane().add(numPad[0]);
+        getContentPane().add(numPad[1]);
+        getContentPane().add(numPad[2]);
+        getContentPane().add(numPad[9]);
+        getContentPane().add(new JPanel());
+        getContentPane().add(numPad[3]);
+        getContentPane().add(numPad[8]);
+        getContentPane().add(new JPanel());
+        getContentPane().add(numPad[4]);
+        getContentPane().add(numPad[7]);
+        getContentPane().add(numPad[6]);
+        getContentPane().add(numPad[5]);
+        new Thread(this).start();
     }
 
     public static void main(String[] args)
     {
         DrehSafe safeSafe = new DrehSafe();
-        safeSafe.setSize(600, 800);
+        safeSafe.setSize(300, 400);
         safeSafe.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e)
+    {
+
+    }
+
+    @Override
+    public void run()
     {
 
     }
