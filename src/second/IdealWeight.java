9228181ec8855a49e2589d3b1ebe225b03d0ea87
Denis Schaffer <schaffer.denis03@gmail.com>
2023-04-07 03:49:19 +0200

    SliderTemp und IdealWeight doku

--- src/second/IdealWeight.java
+++ src/second/IdealWeight.java
@@ -20,28 +20,31 @@ public class IdealWeight extends JFrame implements ActionListener {
     private JLabel resultLabel;
     private JPanel resultPanel;
     private boolean male = true;
-    private float height;
+    private float height = 62f;
 
     public IdealWeight() {
         setTitle("Your Ideal Weight");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Erstellt die beiden Radio-Buttons für Geschlecht (Male und Female) und fügt sie einer ButtonGroup hinzu
         genderM = new JRadioButton("Male", true);
         genderM.setActionCommand("M");
         genderM.addActionListener(this);
         genderF = new JRadioButton("Female", false);
         genderF.setActionCommand("F");
         genderF.addActionListener(this);
-
         genderGroup = new ButtonGroup();
         genderGroup.add(genderM);
         genderGroup.add(genderF);
 
+        // Erstellt ein JPanel für die Geschlechtsauswahl, fügt eine Beschriftung hinzu und die beiden Radio-Buttons
         genderPanel = new JPanel();
         genderPanel.setLayout(new BoxLayout(genderPanel, BoxLayout.Y_AXIS));
         genderPanel.add(new JLabel("Your Gender"));
         genderPanel.add(genderM);
         genderPanel.add(genderF);
 
+        // Erstellt die Radio-Buttons für die Größen-Auswahl und fügt sie einer ButtonGroup hinzu
         heightA = new JRadioButton("60 to 64 inches", true);
         heightA.setActionCommand("A");
         heightA.addActionListener(this);
@@ -57,7 +60,6 @@ public class IdealWeight extends JFrame implements ActionListener {
         heightE = new JRadioButton("76 to 80 inches", false);
         heightE.setActionCommand("E");
         heightE.addActionListener(this);
-
         heightGroup = new ButtonGroup();
         heightGroup.add(heightA);
         heightGroup.add(heightB);
@@ -65,6 +67,7 @@ public class IdealWeight extends JFrame implements ActionListener {
         heightGroup.add(heightD);
         heightGroup.add(heightE);
 
+        // Erstellt ein JPanel für die Größen-Auswahl, fügt eine Beschriftung hinzu und die Radio-Buttons
         heightPanel = new JPanel();
         heightPanel.setLayout(new BoxLayout(heightPanel, BoxLayout.Y_AXIS));
         heightPanel.add(new JLabel("Your Height"));
@@ -74,27 +77,34 @@ public class IdealWeight extends JFrame implements ActionListener {
         heightPanel.add(heightD);
         heightPanel.add(heightE);
 
+        // Erstellt ein JTextField für die Ergebnis-Anzeige und setzt es auf nicht editierbar
         resultText = new JTextField(7);
+        resultText.setText(String.format("%.02f", calculateIdealWeight(male, height)));
         resultText.setEditable(false);
 
+        // Erstellt eine JLabel für das Ergebnis und ein JPanel für die Anzeige des Ergebnisses
         resultLabel = new JLabel("Ideal Weight");
-
         resultPanel = new JPanel();
         resultPanel.add(resultLabel);
         resultPanel.add(resultText);
 
+        // Setzt das Layout des Haupt-Panels auf BorderLayout und fügt die anderen Panels hinzu
         getContentPane().setLayout(new BorderLayout());
         getContentPane().add(genderPanel, BorderLayout.WEST);
         getContentPane().add(heightPanel, BorderLayout.EAST);
         getContentPane().add(resultPanel, BorderLayout.SOUTH);
     }
 
+    // Die main-Methode erzeugt eine Instanz der Klasse IdealWeight, stellt die Größe des Fensters auf 250 x 225 ein
+    // und macht das Fenster sichtbar.
     public static void main(String[] args) {
         IdealWeight weightApp = new IdealWeight();
         weightApp.setSize(250, 225);
         weightApp.setVisible(true);
     }
 
+    // Diese Methode berechnet das ideale Gewicht auf der Grundlage von Geschlecht (männlich/weiblich) und Höhe.
+    // Wenn der Benutzer männlich ist, wird die Formel (Höhe * Höhe) / 30 verwendet, andernfalls (Höhe * Höhe) / 28.
     public static float calculateIdealWeight(boolean male, float height) {
         if (male) {
             return (height * height) / 30;
@@ -103,6 +113,10 @@ public class IdealWeight extends JFrame implements ActionListener {
         }
     }
 
+    // Diese Methode wird aufgerufen, wenn der Benutzer eine Aktion ausführt (z.B. einen RadioButton auswählt).
+    // Die Methode verwendet eine switch-Anweisung, um festzustellen, welche Aktion ausgeführt wurde,
+    // und aktualisiert dann die Variablen male (für Geschlecht) und height (für Höhe) entsprechend.
+    // Schließlich wird das berechnete ideale Gewicht im Textfeld angezeigt.
     @Override
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
@@ -114,6 +128,6 @@ public class IdealWeight extends JFrame implements ActionListener {
             case "D" -> height = 74f;
             case "E" -> height = 78f;
         }
-        resultText.setText("" + calculateIdealWeight(male, height));
+        resultText.setText(String.format("%.02f", calculateIdealWeight(male, height)));
     }
 }
--- src/second/SliderTemp.java
+++ src/second/SliderTemp.java
@@ -5,43 +5,48 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
 
+import first.WordLcm;
+
 /**
  * @author Denis Schaffer, Moritz Binneweiß, Daniel Faigle, Vanessa Schoger, Filip Schepers
  * @version 1, 07/04/2023
  */
 public class SliderTemp extends JFrame implements ChangeListener {
-    JSlider sliderCelsius, sliderFahrenheit;
-    JTextField textCelsius, textFahrenheit;
-    JPanel resultPanel;
+    JSlider sliderCelsius, sliderFahrenheit; // JSliders für Celsius und Fahrenheit
+    JTextField textCelsius, textFahrenheit; // Textfelder für die Anzeige der Werte
+    JPanel resultPanel; // Panel für die Anzeige der Ergebnisse
 
+    //Konstruktor für das Fenster und seine Komponenten.
     public SliderTemp() {
         setTitle("SliderTemp");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         getContentPane().setLayout(new BorderLayout());
-        sliderCelsius = new JSlider(SwingConstants.VERTICAL, -273, 100, 0);
-        sliderCelsius.setMajorTickSpacing(100);
-        sliderCelsius.setMinorTickSpacing(50);
-        sliderCelsius.setPaintTicks(true);
-        sliderCelsius.setPaintLabels(true);
-        sliderCelsius.setPreferredSize(new Dimension(175, 300));
-        sliderCelsius.setBorder(BorderFactory.createTitledBorder("Celsius"));
-        sliderCelsius.addChangeListener(this);
-        sliderCelsius.setName("Celsius");
-        sliderCelsius.setToolTipText("Celsius einstellen");
+
+        // Erstellen des JSliders für Celsius
+        sliderCelsius = new JSlider(SwingConstants.VERTICAL, -273, 105, 0);
+        sliderCelsius.setMajorTickSpacing(21); // Intervall für die großen Ticks (damit auch 0 angezeigt werden kann)
+        sliderCelsius.setPaintTicks(true); // Ticks anzeigen
+        sliderCelsius.setPaintLabels(true); // Label für die Ticks anzeigen
+        sliderCelsius.setPreferredSize(new Dimension(175, 300)); // Größe des JSliders
+        sliderCelsius.setBorder(BorderFactory.createTitledBorder("Celsius")); // Rahmen setzen
+        sliderCelsius.addChangeListener(this); // ChangeListener hinzufügen
+        sliderCelsius.setName("Celsius"); // Namen setzen
+        sliderCelsius.setToolTipText("Celsius einstellen"); // Tooltip setzen
         getContentPane().add(sliderCelsius, BorderLayout.WEST);
 
-        sliderFahrenheit = new JSlider(SwingConstants.VERTICAL, -459, 212, 32);
-        sliderFahrenheit.setMajorTickSpacing(100);
-        sliderFahrenheit.setMinorTickSpacing(50);
-        sliderFahrenheit.setPaintTicks(true);
-        sliderFahrenheit.setPaintLabels(true);
-        sliderFahrenheit.setPreferredSize(new Dimension(175, 300));
-        sliderFahrenheit.setBorder(BorderFactory.createTitledBorder("Fahrenheit"));
-        sliderFahrenheit.addChangeListener(this);
-        sliderFahrenheit.setName("Fahrenheit");
-        sliderFahrenheit.setToolTipText("Fahrenheit einstellen");
+        // Erstellen des JSliders für Fahrenheit
+        sliderFahrenheit = new JSlider(SwingConstants.VERTICAL, -459, 216, 32);
+        sliderFahrenheit.setMajorTickSpacing(27); // Intervall für die großen Ticks (damit auch 0 angezeigt werden kann)
+        sliderFahrenheit.setPaintTicks(true); // Ticks anzeigen
+        sliderFahrenheit.setPaintLabels(true); // Label für die Ticks anzeigen
+        sliderFahrenheit.setPreferredSize(new Dimension(175, 310)); // Größe des JSliders
+        sliderFahrenheit.setBorder(BorderFactory.createTitledBorder("Fahrenheit")); // Rahmen setzen
+        sliderFahrenheit.addChangeListener(this); // ChangeListener hinzufügen
+        sliderFahrenheit.setName("Fahrenheit"); // Namen setzen
+        sliderFahrenheit.setToolTipText("Fahrenheit einstellen"); // Tooltip setzen
         getContentPane().add(sliderFahrenheit, BorderLayout.EAST);
 
+        // Erstellen der Textfelder für die Anzeige der Werte
         textCelsius = new JTextField();
         textCelsius.setText("" + sliderCelsius.getValue());
         textCelsius.setEditable(false);
@@ -50,6 +55,7 @@ public class SliderTemp extends JFrame implements ChangeListener {
         textFahrenheit.setText("" + sliderFahrenheit.getValue());
         textFahrenheit.setEditable(false);
 
+        // Erstellen des Panels für die Anzeige der Ergebnisse
         resultPanel = new JPanel(new GridLayout());
         resultPanel.add(new JLabel(" Celsius:"));
         resultPanel.add(textCelsius);
@@ -60,26 +66,28 @@ public class SliderTemp extends JFrame implements ChangeListener {
 
     public static void main(String[] args) {
         SliderTemp sliderTemp = new SliderTemp();
-        sliderTemp.setSize(362, 500);
+        sliderTemp.setSize(362, 500);// Setze die Größe des Fensters auf 362 x 500 Pixel (Formatierung)
         sliderTemp.setResizable(false);
         sliderTemp.setVisible(true);
     }
 
     @Override
     public void stateChanged(ChangeEvent evt) {
-        JSlider source;
-        source = (JSlider) evt.getSource();
+        // Erstelle ein JSlider-Objekt, das das Event ausgelöst hat
+        JSlider source = (JSlider) evt.getSource();
+        // Wenn der Schieberegler Celsius ist und sich noch in der Veränderung befindet
         if (source.getValueIsAdjusting() && source.getName().equals("Celsius")) {
-            textCelsius.setText("" + sliderCelsius.getValue());
-            int fahrenheit = (int) ((sliderCelsius.getValue() * 1.8) + 32);
+            textCelsius.setText("" + sliderCelsius.getValue());// Setze den Text des JLabels textCelsius auf den aktuellen Wert des Celsius-Schiebereglers
+            int fahrenheit = (int) ((sliderCelsius.getValue() * 1.8) + 32);// Berechne den Wert in Fahrenheit und setze den Text des JLabels textFahrenheit auf den berechneten Wert
             textFahrenheit.setText(String.format("%.02f", ((sliderCelsius.getValue() * 1.8) + 32)));
-            sliderFahrenheit.setValue(fahrenheit);
+            sliderFahrenheit.setValue(fahrenheit);// Setze den Wert des Fahrenheit-Schiebereglers auf den berechneten Wert
         }
+        // Wenn der Schieberegler Fahrenheit ist und sich noch in der Veränderung befindet
         if (source.getValueIsAdjusting() && source.getName().equals("Fahrenheit")) {
-            textFahrenheit.setText("" + sliderFahrenheit.getValue());
-            int celsius = (int) ((sliderFahrenheit.getValue() - 32) / 1.8);
+            textFahrenheit.setText("" + sliderFahrenheit.getValue());// Setze den Text des JLabels textFahrenheit auf den aktuellen Wert des Fahrenheit-Schiebereglers
+            int celsius = (int) ((sliderFahrenheit.getValue() - 32) / 1.8);// Berechne den Wert in Celsius und setze den Text des JLabels textCelsius auf den berechneten Wert
             textCelsius.setText(String.format("%.02f", ((sliderFahrenheit.getValue() - 32) / 1.8)));
-            sliderCelsius.setValue(celsius);
+            sliderCelsius.setValue(celsius);// Setze den Wert des Celsius-Schiebereglers auf den berechneten Wert
         }
     }
 }
