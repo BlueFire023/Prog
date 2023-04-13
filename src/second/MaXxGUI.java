9bddfcdd7606f7c4b3c5d1ed2de092fc492924a2
Moritz-Binneweiß <moritzbinneweiss@gmail.com>
2023-04-13 10:59:06 +0200

    MaXGUI und ToggleSafe start

--- src/second/MaXxGUI.java
+++ src/second/MaXxGUI.java
@@ -1,11 +1,33 @@
 package second;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * @author Denis Schaffer, Moritz Binneweiß, Daniel Faigle, Vanessa Schoger, Filip Schepers
  * @version 1, 13.04.2023
  */
 
-public class MaXxGUI
-{
+public class MaXxGUI extends JFrame implements ActionListener {
+    //Schreiben Sie das Programm MaXxGuI für das „MaXx“-Spiel aus dem 1. Semester (Foliensatz 9, Seite 62).
+    // Es soll dabei möglich sein, mehrere Fenster gleichzeitig offen zu haben und in jedem Fenster ein Spiel zu spielen,
+    // welches von den anderen unabhängig ist.
 
+    public MaXxGUI() {
+        setTitle(" MaXxGUI ");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        MaXxGUI MaXxGUI = new MaXxGUI();
+        MaXxGUI.setSize(800, 600);// Setze die Größe des Fensters auf 800 x 600 Pixel (Formatierung)
+        MaXxGUI.setResizable(false);
+        MaXxGUI.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
--- src/second/ToggleSafe.java
+++ src/second/ToggleSafe.java
@@ -1,11 +1,35 @@
 package second;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * @author Denis Schaffer, Moritz Binneweiß, Daniel Faigle, Vanessa Schoger, Filip Schepers
  * @version 1, 13.04.2023
  */
 
-public class ToggleSafe
-{
+public class ToggleSafe extends JFrame implements ActionListener {
+    // Ändern Sie das DrehSafe-Programm um in ToggleSafe, so dass mehrere aktive Fenster (mit sich drehenden Knöpfen) gleichzeitig offen sein können.
+    // Die Kombination 8-5-2-9-6-3-0-7-4-1 öffnet das Schloss.
+    // Die Drehrichtung ändert sich nach jeweils 9 Schritten.
+    // Bei jedem falschen Knopfdruck öffnet sich ein weiteres Fenster derselben Art, in dem sich die Knöpfe um 33% schneller drehen als im vorherigen Fenster.
+    // Und das Schoss geht erst dann auf, wenn alle Fenster geschlossen wurden
 
+    public ToggleSafe() {
+        setTitle(" ToggleSafe ");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        ToggleSafe ToggleSafe = new ToggleSafe();
+        ToggleSafe.setSize(800, 600);// Setze die Größe des Fensters auf 800 x 600 Pixel (Formatierung)
+        ToggleSafe.setResizable(false);
+        ToggleSafe.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
