8cb0dd3e4865c4e5a863fb8da3ec5d74a4de6bb1
Moritz-Binneweiß <moritzbinneweiss@gmail.com>
2023-04-13 10:53:03 +0200

    MyPaint Fenster erstellt

--- src/second/MyPaint.java
+++ src/second/MyPaint.java
@@ -1,11 +1,32 @@
 package second;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * @author Denis Schaffer, Moritz Binneweiß, Daniel Faigle, Vanessa Schoger, Filip Schepers
  * @version 1, 13.04.2023
  */
 
-public class MyPaint
-{
+public class MyPaint extends JFrame implements ActionListener {
+    //Als Ergebnis sollte ein Programm MyPaint entstehen und abgegeben werden, das es erlaubt,
+    // einfache Malereien auf einen 2-dimensionalen Gitter zu erzeugen (Phantasie erlaubt / gefragt)
 
+    public MyPaint() {
+        setTitle(" MyPaint ");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        MyPaint MyPaint = new MyPaint();
+        MyPaint.setSize(800, 600);// Setze die Größe des Fensters auf 800 x 600 Pixel (Formatierung)
+        MyPaint.setResizable(false);
+        MyPaint.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
