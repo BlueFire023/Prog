1eee11f91049480eda869e872fba224d9f090254
Moritz-Binneweiß <moritzbinneweiss@gmail.com>
2023-04-13 10:40:10 +0200

    Klassen erstellt

---
+++ src/second/ToggleSafe.java
@@ -0,0 +1,11 @@
+package second;
+
+/**
+ * @author Denis Schaffer, Moritz Binneweiß, Daniel Faigle, Vanessa Schoger, Filip Schepers
+ * @version 1, 13.04.2023
+ */
+
+public class ToggleSafe
+{
+
+}
