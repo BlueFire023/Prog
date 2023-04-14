36379c6ea9ff5a9649ec4150281c42057dfaa639
Denis Schaffer <schaffer.denis03@gmail.com>
2023-04-14 12:08:37 +0200

    MyPaint improved

--- src/second/MyPaint.java
+++ src/second/MyPaint.java
@@ -3,7 +3,7 @@ package second;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Random;
+import java.util.Stack;
 
 /**
  * @author Denis Schaffer, Moritz Binneweiß, Daniel Faigle, Vanessa Schoger, Filip Schepers
@@ -12,8 +12,23 @@ import java.util.Random;
 public class MyPaint extends JFrame implements MouseMotionListener, MouseListener, ActionListener {
     private final int WIDTH = 128;
     private final int HEIGHT = 128;
+    private boolean fillMode = false;
     private JPanel canvas = new JPanel();
-    private JPanel colorPicker = new JPanel();
+    private JPanel toolBar = new JPanel();
+    JButton[] buttons = {
+            new JButton("Black"),
+            new JButton("Blue"),
+            new JButton("Cyan"),
+            new JButton("Gray"),
+            new JButton("Green"),
+            new JButton("Magenta"),
+            new JButton("Orange"),
+            new JButton("Pink"),
+            new JButton("Red"),
+            new JButton("White"),
+            new JButton("Yellow")
+    };
+    JButton prevButton = buttons[0];
     private JPanel[][] pixels;
     private final Color[] COLORS = {Color.black, Color.blue, Color.cyan, Color.gray, Color.green, Color.magenta, Color.orange, Color.pink, Color.red, Color.white, Color.yellow};
     private int currentColorIndex = 0;
@@ -38,34 +53,34 @@ public class MyPaint extends JFrame implements MouseMotionListener, MouseListene
         }
         getContentPane().add(canvas);
 
-        JButton[] buttons = {
-                new JButton("Black"),
-                new JButton("Blue"),
-                new JButton("Cyan"),
-                new JButton("Gray"),
-                new JButton("Green"),
-                new JButton("Magenta"),
-                new JButton("Orange"),
-                new JButton("Pink"),
-                new JButton("Red"),
-                new JButton("White"),
-                new JButton("Yellow")
-        };
+
         int i = 0;
         for (JButton a : buttons) {
             add(a);
-            colorPicker.add(a);
+            toolBar.add(a);
             a.setActionCommand("" + i++);
+            a.setFont(new Font("Courier", Font.BOLD, 10));
+            a.setBackground(Color.WHITE);
             a.addActionListener(this);
         }
         JButton clearButton = new JButton("Clear");
         clearButton.addActionListener(this);
-        colorPicker.add(clearButton);
-        colorPicker.setLayout(new GridLayout(3, 5));
-        getContentPane().add(colorPicker);
+        clearButton.setFont(new Font("Courier", Font.BOLD, 10));
+        clearButton.setBackground(Color.WHITE);
+        toolBar.add(clearButton);
+
+        JButton fillTool = new JButton("Fill");
+        fillTool.setFont(new Font("Courier", Font.BOLD, 10));
+        fillTool.setBackground(Color.WHITE);
+        fillTool.addActionListener(this);
+        toolBar.add(fillTool);
+        toolBar.setLayout(new GridLayout(3, 5));
+        getContentPane().add(toolBar);
         getContentPane().validate();
         addMouseMotionListener(this);
         addMouseListener(this);
+        buttons[0].setBackground(Color.darkGray);
+        buttons[0].setForeground(Color.WHITE);
         setVisible(true);
     }
 
@@ -124,6 +139,11 @@ public class MyPaint extends JFrame implements MouseMotionListener, MouseListene
     public void mousePressed(MouseEvent e) {
         prevY = e.getX() / (640 / WIDTH);
         prevX = e.getY() / (640 / HEIGHT) - 7;
+        if (fillMode && prevX > 0 && prevX < 128 && prevY > 0 && prevY < 128) {
+            fillTool(prevX, prevY);
+        } else if (prevX > 0 && prevX < 128 && prevY > 0 && prevY < 128) {
+            pixels[prevX][prevY].setBackground(COLORS[currentColorIndex]);
+        }
     }
 
     @Override
@@ -143,14 +163,52 @@ public class MyPaint extends JFrame implements MouseMotionListener, MouseListene
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
         if (((JButton) e.getSource()).getText().equals("Clear")) {
             for (int i = 0; i < WIDTH; i++) {
                 for (int j = 0; j < HEIGHT; j++) {
                     pixels[i][j].setBackground(Color.WHITE);
                 }
             }
+        } else if (((JButton) e.getSource()).getText().equals("Fill")) {
+            fillMode = !fillMode;
+            if (fillMode) {
+                ((JButton) e.getSource()).setBackground(Color.darkGray);
+                ((JButton) e.getSource()).setForeground(Color.white);
+            } else {
+                ((JButton) e.getSource()).setBackground(Color.WHITE);
+                ((JButton) e.getSource()).setForeground(Color.BLACK);
+            }
         } else {
             currentColorIndex = Integer.parseInt(e.getActionCommand());
+            prevButton.setBackground(Color.WHITE);
+            prevButton.setForeground(Color.BLACK);
+            ((JButton) e.getSource()).setBackground(Color.darkGray);
+            ((JButton) e.getSource()).setForeground(Color.white);
+            prevButton = (JButton) e.getSource();
         }
     }
+
+    public void fillTool(int x, int y) {
+        Color targetColor = pixels[x][y].getBackground();
+        Color replacementColor = COLORS[currentColorIndex];
+        if (!targetColor.equals(replacementColor)) {
+            Stack<Point> stack = new Stack<>();
+            stack.push(new Point(x, y));
+
+            while (!stack.isEmpty()) {
+                Point p = stack.pop();
+                int i = p.x;
+                int j = p.y;
+                if (i >= 0 && i < WIDTH && j >= 0 && j < HEIGHT && pixels[i][j].getBackground().equals(targetColor)) {
+                    pixels[i][j].setBackground(replacementColor);
+                    stack.push(new Point(i + 1, j));
+                    stack.push(new Point(i - 1, j));
+                    stack.push(new Point(i, j + 1));
+                    stack.push(new Point(i, j - 1));
+                }
+            }
+        }
+    }
+
 }
