aa414334fe19b7d39ae2c6630fce5edfc08a0883
Filip <filip.schepers2@gmail.com>
2023-05-05 18:13:00 +0200

    standard Text

--- src/second/Umfuelllisten.java
+++ src/second/Umfuelllisten.java
@@ -7,27 +7,29 @@ package second;
 import java.util.*;
 
 public class Umfuelllisten {
-    static int[] capacities = new int[5];
+    static int[] kapazität = new int[5]; //Größen der Eimer
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // Einlesen der Kapazitäten
         for (int i = 0; i < 5; i++) {
-            capacities[i] = scanner.nextInt();
+            kapazität[i] = scanner.nextInt();
         }
-        int[] initial = {capacities[0], 0, 0, 0, 0};
-        List<int[]> states = getAllStates(capacities, initial);
+        int[] initial = {kapazität[0], 0, 0, 0, 0}; //Startzustand
+        List<int[]> states = getAllStates(kapazität, initial);
         for (int[] state : states) {
             System.out.println(Arrays.toString(state));
         }
         System.out.println(states.size() + " states");
     }
 
+    //Errechnen aller Zustände
     public static List<int[]> getAllStates(int[] capacities, int[] initial) {
-        Set<String> visited = new HashSet<>();
+        Set<String> visited = new HashSet<>(); //Set mit bereits genutzten Zuständen
         List<int[]> result = new ArrayList<>();
-        dfs(capacities, initial, visited, result);
+        umfüllen(capacities, initial, visited, result);
+        //Sortieren der Liste
         Collections.sort(result, (a, b) -> {
             for (int i = 0; i < a.length; i++) {
                 if (a[i] != b[i]) {
@@ -39,7 +41,8 @@ public class Umfuelllisten {
         return result;
     }
 
-    private static void dfs(int[] capacities, int[] current, Set<String> visited, List<int[]> result) {
+    //Umfüllen der Eimer mithilfe der Regeln
+    private static void umfüllen(int[] capacities, int[] current, Set<String> visited, List<int[]> result) {
         String state = Arrays.toString(current);
         if (visited.contains(state)) {
             return;
@@ -56,7 +59,7 @@ public class Umfuelllisten {
                         if (amount > 0) {
                             current[i] -= amount;
                             current[j] += amount;
-                            dfs(capacities, current, visited, result);
+                            umfüllen(capacities, current, visited, result);
                             current[i] += amount;
                             current[j] -= amount;
                         }
