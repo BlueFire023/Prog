b39fe3050037eea5d256d524a185518be5f67b21
MoRitZ516 <moritz.binne@gmail.com>
2023-05-11 13:08:20 +0200

    Inspector: Klassen,Superklassen, Interfaces einlesen + Kommentare dazu

--- src/second/Inspector.java
+++ src/second/Inspector.java
@@ -5,23 +5,42 @@ package second;
  * @version 1, 11/05/2023
  */
 
-import java.lang.reflect.*;
 import java.util.Scanner;
 
 public class Inspector {
     public static void main(String[] args) {
+        //Benutzereingabe der gewünschten Klasse
         System.out.println("Geben sie hier den Namen der gewünschten Klasse ein: ");
         Scanner scanLeser = new Scanner(System.in);
         String klassenName = scanLeser.nextLine();
 
         while (true) {
             try {
+                //Laufzeit über Benutzereingabe beenden
                 if (klassenName.toLowerCase().startsWith("end")) {
                     break;
                 }
+                //Klassenobjekt anhand des Klassennamens erstellen
                 Class<?> klassenObjekt = Class.forName(klassenName);
 
-            } catch (ClassNotFoundException e) {
+                //Superklasse ausgeben
+                Class<?> superklassenObjekt = klassenObjekt.getSuperclass();
+                if (superklassenObjekt != null){
+                    System.out.println("Superklasse: " + superklassenObjekt.getName());
+                }
+
+                //Interfaces ausgeben
+                Class<?>[] interfaceObjekte = klassenObjekt.getInterfaces();
+                if (interfaceObjekte.length > 0){
+                    System.out.println("Interfaces: ");
+                    for (Class<?> inface: interfaceObjekte){
+                        System.out.println(inface.getName());
+                    }
+                }
+
+            }
+            //Benutzereingabe konnte nicht gefunden werden
+            catch (ClassNotFoundException e) {
                 System.out.println("Klasse("+klassenName+") wurde nicht gefunden.");
             }
         }
